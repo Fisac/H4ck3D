0b14c720c32f8ee5d864f06252c0a12cd0b9e7a5
Joakim <jokenordstrom@hotmail.com>
2018-09-13 13:55:52 +0200

    Added matterPopup on object rayOver

---
+++ Assets/Scripts/Objects/LocalScaling.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalScaling : MonoBehaviour {
+
+	// Update is called once per frame
+	void Start () {
+        transform.localScale = transform.localScale;
+	}
+}
--- Assets/Scripts/Objects/LookAtCamera.cs
+++ Assets/Scripts/Objects/LookAtCamera.cs
@@ -6,32 +6,36 @@ using UnityEngine.UI;
 public class LookAtCamera : MonoBehaviour {
 
     public Camera vrCamera;
+    public InteractableObject interactableObject;
+
+    Transform myTransform;
+    public Transform boxTransform;
 
     GameObject canvasHolder;
-    public Text popupName;
     public Text popupMatter;
 
-    string nameOfParent;
-    
+    // Use this for initialization
+    void Start () {
+        myTransform = GetComponent<Transform>();
 
-	// Use this for initialization
-	void Start () {
+        myTransform.localScale = new Vector3(myTransform.localScale.x, myTransform.localScale.y, boxTransform.localScale.z * 2.25f);
+        myTransform.position = new Vector3(boxTransform.position.x, boxTransform.position.y, boxTransform.position.z);
 
-        nameOfParent = GetComponentInParent<GameObject>().name;
-        popupName = GetComponentInChildren<Text>();
-
-        popupMatter.text = GetComponentInParent<InteractableObject>().matter.name;
-        popupName.text = nameOfParent;
-
-        if (FindObjectOfType<Camera>().tag=="MainCamera")
+        if (FindObjectOfType<Camera>().tag == "MainCamera")
         {
-            vrCamera = GetComponent<Camera>();	
+            vrCamera = GetComponent<Camera>();
         }
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
         transform.LookAt(vrCamera.transform.position);
+
+        UpdateText();
     }
 
+    void UpdateText()
+    {
+        popupMatter.text = interactableObject.matter.name;
+    }
 }
