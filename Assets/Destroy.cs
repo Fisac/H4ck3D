646a4196c63cac94b2cb4007b6fd50f2c2f6b6e0
josefina <31647229+Raseration@users.noreply.github.com>
2018-09-17 13:24:27 +0200

    Destroy
    Man kan nu förstöra en interactable genom att trycka på den

--- Assets/Destroy.cs
+++ Assets/Destroy.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class Destroy : MonoBehaviour {
 
+    private float objectnumber;
+    public GameObject destroyed;
+    public InteractableObject interactableObject;
+    public MeshRenderer mesh;
+
 	// Use this for initialization
-	void Start ()
+	void OnMouseDown()
     {
-		
-	}
+        float boxVolume = interactableObject.boxVolume;
+        objectnumber = boxVolume;
+
+        print(objectnumber);
+        for (int i = 0; i <= objectnumber; i++)
+        { 
+            Instantiate(destroyed, transform.position, transform.rotation);
+        }
+        mesh.enabled = false;
+    }
 	
 	// Update is called once per frame
 	void Update ()
