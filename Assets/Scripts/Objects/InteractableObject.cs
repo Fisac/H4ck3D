6a1a39d1fd8411a8f3057df121f7e95d2ca70fec
Joakim <jokenordstrom@hotmail.com>
2018-09-17 13:02:38 +0200

    dumdidum

--- Assets/Scripts/Objects/InteractableObject.cs
+++ Assets/Scripts/Objects/InteractableObject.cs
@@ -19,7 +19,7 @@ public class InteractableObject : MonoBehaviour {
 
     public float mass;
     public float maximumLiftWeight = 4;
-    float boxVolume;
+    public float boxVolume;
 
     bool newMatter;
 
@@ -34,6 +34,12 @@ public class InteractableObject : MonoBehaviour {
         {
             UpdateProperties();
         }
+    }
+
+    private void FixedUpdate()
+    {
+        //If(grabbable)
+        //Force = mass * acceleration, acceleration = units/(second*second) or (currentVelocity-startVelocity)/timeBetweenReadings
     }
 
     void UpdateMatter(Matter matter)
