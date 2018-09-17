21ea76788b9b0bc3d82eaede91090429f73d5c07
Joakim <jokenordstrom@hotmail.com>
2018-09-17 15:56:12 +0200

    gAAAAHHHAHHAH

--- Assets/Scripts/ForceCheck.cs
+++ Assets/Scripts/ForceCheck.cs
@@ -12,14 +12,25 @@ public class ForceCheck : MonoBehaviour
         interactableObject = GetComponent<InteractableObject>();
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("Collision!");
-        if (other.GetComponent<InteractableObject>().force > interactableObject.matter.breakingPoint/* && interactableObject.matter.isDestructable*/)
-        {
-            //I am destroyed
-            //destroyGlass.DestroyTheGlass();
-            Debug.Log("Collision between " + other.name + "and " + this.name + " with a force of " + other.GetComponent<InteractableObject>().force);
-        }
-    }
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    Debug.Log("Collision!");
+    //    if (other.GetComponent<InteractableObject>().force > interactableObject.matter.breakingPoint/* && interactableObject.matter.isDestructable*/)
+    //    {
+    //        //I am destroyed
+    //        //destroyGlass.DestroyTheGlass();
+    //        Debug.Log("Collision between " + other.name + "and " + this.name + " with a force of " + other.GetComponent<InteractableObject>().force);
+    //    }
+        
+    //}
+    //private void OnCollisionEnter(Collision other)
+    //{
+    //    Debug.Log("Collision!");
+    //    if (other.relativeVelocity.magnitude > interactableObject.matter.breakingPoint/* && interactableObject.matter.isDestructable*/)
+    //    {
+    //        //I am destroyed
+    //        //destroyGlass.DestroyTheGlass();
+    //        Debug.Log("Collision between " + other.gameObject.name + "and " + this.name + " with a force of " + other.relativeVelocity.magnitude);
+    //    }
+    //}
 }
