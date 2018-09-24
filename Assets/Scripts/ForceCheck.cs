88a90d231cf8db3c7e238fbab2bb140ab2437f87
Fisac <isac_degerborg@hotmail.com>
2018-09-24 15:57:48 +0200

    Edits to the UI and VRTK prefab

--- Assets/Scripts/ForceCheck.cs
+++ Assets/Scripts/ForceCheck.cs
@@ -15,26 +15,18 @@ public class ForceCheck : MonoBehaviour
 
     private void OnCollisionEnter(Collision other)
     {
-        //if (!other.transform.GetComponentInChildren<InteractableObject>())
-        //{
-        //    return;
-        //}
+        InteractableObject otherInteractableObject = other.transform.GetComponentInChildren<InteractableObject>();
 
-        if (!other.transform.GetComponentInChildren<InteractableObject>() && interactableObject.force > interactableObject.matter.breakingPoint && interactableObject.matter.isDestructable)
+        if (!otherInteractableObject && interactableObject.force > interactableObject.matter.breakingPoint && interactableObject.matter.isDestructable)
         {
-            Debug.Break();
             destroyGlass.DestroyTheGlass();
         }
-        else if (other.transform.GetComponentInChildren<InteractableObject>().force > interactableObject.matter.breakingPoint && interactableObject.matter.isDestructable)
+        else if (otherInteractableObject && otherInteractableObject.force > interactableObject.matter.breakingPoint && interactableObject.matter.isDestructable)
         {
-            Debug.Break();
-
             destroyGlass.DestroyTheGlass();
         }
-        else if (other.transform.GetComponentInChildren<InteractableObject>() && interactableObject.force > interactableObject.matter.breakingPoint && interactableObject.matter.isDestructable)
+        else if (otherInteractableObject && interactableObject.force > interactableObject.matter.breakingPoint && interactableObject.matter.isDestructable)
         {
-            Debug.Break();
-
             destroyGlass.DestroyTheGlass();
         }
         else
