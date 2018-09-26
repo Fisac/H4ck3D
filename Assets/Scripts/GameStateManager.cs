ee074dad6e8ca21f3983ac570185e19a7a231ab1
Unknown <chris_albin@outlook.com>
2018-09-26 11:37:34 +0200

    Added GameStateManager (Level Load and Save) + Lock + Metal Color

---
+++ Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStateManager : MonoBehaviour {
+
+    Scene currentScene;
+    SceneManager sceneManager;
+    string levelToLoad;
+
+
+    // Use this for initialization
+    private void Awake()
+    {
+        currentScene = SceneManager.GetActiveScene();
+
+        if(currentScene.buildIndex > 0)
+        {  
+            SaveCurrentLevel();
+        }
+        else
+        {
+            return; 
+        }
+    }
+
+    public void SaveCurrentLevel()
+    {
+        PlayerPrefs.SetString("LevelToLoad", currentScene.name);
+        print("Saving Scene" + PlayerPrefs.GetString("LevelToLoad)"));
+    }
+
+    public void LoadLevel()
+    {
+        levelToLoad = PlayerPrefs.GetString("LevelToLoad");
+        print(levelToLoad);
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
